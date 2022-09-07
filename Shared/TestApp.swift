c9c5b48a05951d14eb00c2798866422894317cdd
AlaraSoftworks <alarasoftworks@gmail.com>
2022-09-07 15:59:04 +1000

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Robert Charlton on 7/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
